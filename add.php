6581ddc45d18b32d4803c0894c73946ce9c1159e
Marcelka96 <46938739+Marcelka96@users.noreply.github.com>
2019-01-22 21:06:14 +0100

    V1.0
    Poprawki błędów. Wersja finałowa

--- add.php
+++ add.php
@@ -16,13 +16,13 @@
 
 		$query->bindParam(':one',$_GET['one'], PDO::PARAM_STR);
 		$query->bindParam(':two',$_GET['two'], PDO::PARAM_STR);
-        $query->bindParam(':three',$_GET['three'], PDO::PARAM_STR);
-        $query->bindParam(':four',$_GET['four'], PDO::PARAM_STR);
-        $query->bindParam(':five',$_GET['five'], PDO::PARAM_STR);
-        $query->bindParam(':six',$_GET['six'], PDO::PARAM_STR);
-        $query->bindParam(':sewen',$_GET['seven'], PDO::PARAM_STR);
-        $query->bindParam(':eigt',$_GET['eigt'], PDO::PARAM_STR);
-        $query->bindParam(':nine',$_GET['nine'], PDO::PARAM_STR);
-        $query->bindParam(':ten',$_GET['ten'], PDO::PARAM_STR);
+        	$query->bindParam(':three',$_GET['three'], PDO::PARAM_STR);
+        	$query->bindParam(':four',$_GET['four'], PDO::PARAM_STR);
+        	$query->bindParam(':five',$_GET['five'], PDO::PARAM_STR);
+        	$query->bindParam(':six',$_GET['six'], PDO::PARAM_STR);
+        	$query->bindParam(':seven',$_GET['seven'], PDO::PARAM_STR);
+        	$query->bindParam(':eight',$_GET['eight'], PDO::PARAM_STR);
+        	$query->bindParam(':nine',$_GET['nine'], PDO::PARAM_STR);
+        	$query->bindParam(':ten',$_GET['ten'], PDO::PARAM_STR);
 		$query->execute();
 		header('Location: index.html');
